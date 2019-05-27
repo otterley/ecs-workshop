498b26409790bf81782459d2f4fbd57b3856b214
Michael S. Fischer <fiscmi@amazon.com>
2019-05-27 11:34:30 -0400

    WIP

--- classroom/set-credentials.sh
+++ classroom/set-credentials.sh
@@ -2,22 +2,13 @@
 
 set -e
 
-echo -e "username\tpassword\taws_access_key_id\taws_secret_access_key"
+echo -e "username\tpassword"
 
-for i in $(seq 0 10); do
+for i in $(seq 0 13); do
     username="ecs-workshop-user-$i"
     password=$(openssl rand 10 | base64 | sed -e 's/==$//')
     # Set console password
-    aws iam update-login-profile --user-name "$username" --password "$password" --no-password-reset-required >/dev/null
-    # Delete access keys first
-    oldKeys=$(aws iam list-access-keys --user-name "$username" --query 'AccessKeyMetadata[].AccessKeyId' --output text)
-    if [[ "$oldKeys" != None ]]; then
-        for oldKey in $oldKeys; do
-            aws iam delete-access-key --user-name "$username" --access-key-id "$oldKey"
-        done
-    fi
-    creds=$(aws iam create-access-key --user-name "$username")
-    accessKeyId=$(echo $creds | jq -r .AccessKey.AccessKeyId)
-    secretAccessKey=$(echo $creds | jq -r .AccessKey.SecretAccessKey)
-    echo -e "${username}\t${password}\t${accessKeyId}\t${secretAccessKey}"
+    aws iam create-login-profile --user-name "$username" --password "$password" --no-password-reset-required >/dev/null 2>&1 || \
+       aws iam update-login-profile --user-name "$username" --password "$password" --no-password-reset-required >/dev/null
+    echo -e "${username}\t${password}"
 done
