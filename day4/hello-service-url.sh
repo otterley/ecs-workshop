15bedd75874236b1da402e5017fd6886430b3d98
Michael S. Fischer <fiscmi@amazon.com>
2019-05-29 18:06:54 -0400

    WIP

---
+++ day4/hello-service-url.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+url=$(aws cloudformation describe-stacks --stack-name ${C9_USER}-hello-service --query 'Stacks[0].Outputs[?OutputKey==`Url`].OutputValue' --output text)
+echo "Click here: http://$url"
