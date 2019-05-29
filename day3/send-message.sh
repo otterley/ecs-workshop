baf8af0665ff8e3f32ea042cf367c43acc9e68c7
Michael S. Fischer <fiscmi@amazon.com>
2019-05-28 21:08:01 -0400

    WIP

--- day3/send-message.sh
+++ day3/send-message.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 
-taskDefinition=$(aws cloudformation list-exports --query "Exports[?Name==`$C9_USER:MessageTaskDefinition`].Value" --output text)
+taskDefinition=$(aws cloudformation list-exports --query "Exports[?Name==\`$C9_USER:MessageTaskDefinition\`].Value" --output text)
 
 aws ecs run-task --cluster $C9_USER --task-definition $taskDefinition
