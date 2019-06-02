8ff0b69023d26e822b0a3c05ddb14605e60fe682
Michael S. Fischer <fiscmi@amazon.com>
2019-06-01 20:43:49 -0700

    WIP

--- classroom/create-classroom.sh
+++ classroom/create-classroom.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
 defaultPassword=$(openssl rand 10 | base64 | sed -e 's/==$//')
+createUsers=${CREATE_USERS:=true}
 
 aws cloudformation deploy \
   --template-file classroom.yml \
   --stack-name ecs-clsrm-DO-NOT-DELETE \
-  --parameter-overrides DefaultPassword=${defaultPassword} \
+  --parameter-overrides \
+      DefaultPassword=${defaultPassword} \
+      CreateUsers=${createUsers} \
   --capabilities CAPABILITY_NAMED_IAM
